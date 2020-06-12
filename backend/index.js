a9d87f36f27d1e23c96ceda623d28e6808afe9d0
Meredith Noelani Burkle <mburkle@apple.com>
2020-06-11 18:37:15 -0700

    adding generations and changing the index to have a new generation and new dragon class

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,45 @@
-// console.log("Hello Node Friend");
+const Generation = require('./generation');
+
+const generation = new Generation();
+
+console.log('generation', generation);
+
+const grant = generation.newDragon();
+
+console.log('grant: ', grant);
+
+setTimeout(() => {
+    const mimar = generation.newDragon();
+    console.log('mimar', mimar);
+}, 15000);
+
+
+// from before we had the generation stuff:
 
 // setting this dragon variable to the 
 // result of the module.exports line of 
 // the dragons.js file
-const Dragon = require('./dragon.js');
 
-const friend = new Dragon({
-    birthdate: new Date(), 
-    nickname: 'friend'
-});
-const balloon = new Dragon({
-    nickname: 'balloon',
-    birthdate: new Date()
-});
+// const Dragon = require('./dragon.js');
 
-const meepy = new Dragon();
+// const friend = new Dragon({
+//     birthdate: new Date(), 
+//     nickname: 'friend'
+// });
+// const balloon = new Dragon({
+//     nickname: 'balloon',
+//     birthdate: new Date()
+// });
 
-// first parameter is a callback function
-// second parameter is the delay
-setTimeout(() => {
-    const grant = new Dragon();
-    console.log('grant', grant);
-}, 3000);
+// const meepy = new Dragon();
 
-console.log('friend', friend);
-console.log('balloon', balloon);
-console.log('meepy', meepy);
+// // first parameter is a callback function
+// // second parameter is the delay
+// setTimeout(() => {
+//     const grant = new Dragon();
+//     console.log('grant', grant);
+// }, 3000);
+
+// console.log('friend', friend);
+// console.log('balloon', balloon);
+// console.log('meepy', meepy);
