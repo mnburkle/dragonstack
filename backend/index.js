872c7256c9f02fe2c7fa10ae022515286f95ce01
Meredith Noelani Burkle <mburkle@apple.com>
2020-06-15 22:43:59 -0700

    express web requests :D

--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,23 @@
+const express = require('express'); // don't have to point to the directory, it'll find it in node modules
 const GenerationEngine = require('./generation_engine.js');
 
+// sets up the object that is a representation of the web server
+// start and stop web server and requests
+const app = express();
 const engine = new GenerationEngine();
+const port = 3000;
+
 engine.start();
 
-setTimeout(() => {
-    engine.stop()
-}, 20000);
+app.get('/dragon/new/', (req, res) => {
+    res.json({ dragon: engine.generation.newDragon() });
+}); // takes endpoint
 
-// console.log('generation', generationEngine);
 
-// const generation = new Generation();
-
-// console.log('generation', generation);
-
-// const grant = generation.newDragon();
-
-// console.log('grant: ', grant);
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+}); // listen for requests at the particular port, 3000
 
 // setTimeout(() => {
-//     const mimar = generation.newDragon();
-//     console.log('mimar', mimar);
-// }, 15000);
-
-
-// from before we had the generation stuff:
-
-// setting this dragon variable to the 
-// result of the module.exports line of 
-// the dragons.js file
-
-// const Dragon = require('./dragon.js');
-
-// const friend = new Dragon({
-//     birthdate: new Date(), 
-//     nickname: 'friend'
-// });
-// const balloon = new Dragon({
-//     nickname: 'balloon',
-//     birthdate: new Date()
-// });
-
-// const meepy = new Dragon();
-
-// // first parameter is a callback function
-// // second parameter is the delay
-// setTimeout(() => {
-//     const grant = new Dragon();
-//     console.log('grant', grant);
-// }, 3000);
-
-// console.log('friend', friend);
-// console.log('balloon', balloon);
-// console.log('meepy', meepy);
+//     engine.stop()
+// }, 20000);
