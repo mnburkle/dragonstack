8eab2f2414c10a44d3afa0593710a577d75b82f2
mnburkle <mnburkle@stanford.edu>
2020-09-19 12:34:58 -0700

    tries to fetch generation, but it fails with control allow origin error

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -5,12 +5,25 @@ class Generation extends Component {
     // equivalent to setting "this.state" within a constructor
     state = { generation: { generationId: -1, expiration: '2021-01-01' } };
 
+    // so these are like hooks i guess
+    componentDidMount() {
+        this.fetchGeneration();
+    }
+    
+    fetchGeneration = () => {
+        // special function exposed for javascript, pass in url, returns a promise
+        fetch('http://localhost:3000/generation')
+            .then((response) => {
+                console.log('response', response); 
+            });
+    };
+
     // we have to have this
     render() {
         // describe component structure of this component through jsx
         const { generation } = this.state;
         // ^ destructuring syntax equivalent to const generation = this.state.generation;
-        
+
         return (
             <div>
                 <h3>Generation {generation.generationId}. Expires on:</h3>
