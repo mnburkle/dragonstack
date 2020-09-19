f2af608501e6ea097ad23f8940bfeb118e94783c
mnburkle <mnburkle@stanford.edu>
2020-09-19 12:29:40 -0700

    shift around so we have a state

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 // inherits a bunch of methods and features from component
 class Generation extends Component {
+    // equivalent to setting "this.state" within a constructor
+    state = { generation: { generationId: -1, expiration: '2021-01-01' } };
+
     // we have to have this
     render() {
         // describe component structure of this component through jsx
-        const generation = { generationId: -1, expiration: '2021-01-01'}
+        const { generation } = this.state;
+        // ^ destructuring syntax equivalent to const generation = this.state.generation;
+        
         return (
             <div>
                 <h3>Generation {generation.generationId}. Expires on:</h3>
