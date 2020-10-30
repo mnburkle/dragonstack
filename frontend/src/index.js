b74a2c6dfeeb6bfbe3bfed824971376343de44e9
mnburkle <mnburkle@stanford.edu>
2020-10-29 22:10:00 -0700

    up through video 82

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,10 @@ import Dragon from './components/Dragon';
 import './index.css';
 
 const DEFAULT_GENERATION = { generationId: '', expiration: ''};
+const GENERATION_ACTION_TYPE = 'GENERATION_ACTION_TYPE';
 
 const generationReducer = (state, action) => {
-    console.log('generationreducer state', state);
-    console.log('generationreducer action', action);
-
-    if (action.type === 'GENERATION_ACTION_TYPE') {
+    if (action.type === GENERATION_ACTION_TYPE) {
         return { generation: action.generation };
     }
 
@@ -25,16 +23,49 @@ const generationReducer = (state, action) => {
 
 const store = createStore(generationReducer);
 
+// gotta do this before the dispatch. need to register with redux
+// before actions are dispatched
+store.subscribe(() => {
+    console.log('store state update', store.getState());
+});
+
+console.log('store', store);
 
 store.dispatch({ type: 'foo' });
 
 // type, generation payload
 store.dispatch({ 
-    type: 'GENERATION_ACTION_TYPE' ,
+    type: GENERATION_ACTION_TYPE ,
     generation: { generation: 'goo', expiration: 'bar'}
 });
 
 console.log('store get state at the end', store.getState());
+
+// generation data that will be placed within action object
+// action creator is not the action itself
+// the inner object it returns is action
+// this is wrapper around action. 
+const generationActionCreator = (payload) => {
+    return {
+        type: GENERATION_ACTION_TYPE,
+        generation: payload
+    };
+}
+
+const zooAction = generationActionCreator({
+    generationId: 'zoo',
+    expiration: 'bar'
+});
+
+store.dispatch(zooAction);
+
+fetch('http://localhost:3000/generation')
+    .then(response => {
+        response.json()
+            .then(json => {
+                store.dispatch(generationActionCreator(json.generation));
+            });
+    });
 
 // jsx syntax
 render(
