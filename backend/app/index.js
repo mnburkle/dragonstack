6a386660b85bbe27035badf3b3a0aa816ceb6ecd
mnburkle <mnburkle@stanford.edu>
2020-12-01 22:08:23 -0800

    configuring postman and then fixing a bug. i think it had to do with an extra comma in the accountsql table

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,9 +1,10 @@
 const express = require('express'); // don't have to point to the directory, it'll find it in node modules
 const cors = require('cors'); // function which returns the cors middleware. express will use it
-
+const bodyParser = require('body-parser');
 const GenerationEngine = require('./generation/engine.js');
 const dragonRouter = require('./api/dragon');
 const generationRouter = require('./api/generation');
+const accountRouter = require('./api/account');
 
 // sets up the object that is a representation of the web server
 // start and stop web server and requests
@@ -14,7 +15,9 @@ app.locals.engine = engine;
 
 // now identify backend server to have same origin as front end
 app.use(cors({ origin: 'http://localhost:1234' }));
+app.use(bodyParser.json());
 
+app.use('/account', accountRouter);
 app.use('/dragon', dragonRouter); // attach all routes defined in dragon file, on the /dragon/ subroute
 app.use('/generation', generationRouter);
 
--- backend/bin/sql/account.sql
+++ backend/bin/sql/account.sql
@@ -1,5 +1,5 @@
 CREATE TABLE account(
     id             SERIAL PRIMARY KEY, 
     username       CHARACTER(64),
-    password       CHARACTER(64), 
+    password       CHARACTER(64)
 );
