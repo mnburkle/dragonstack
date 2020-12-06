c557ff6739792f362eff5153550f669c8a200e51
mnburkle <mnburkle@stanford.edu>
2020-12-05 20:43:51 -0800

    103. hash sensitive information

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 const AccountTable = require('../account/table.js');
+const { hash } = require('../account/helper');
 
 const router = new Router();
 
 router.post('/signup', (req, res, next) => {
     const { username, password } = req.body;
+    const usernameHash = hash(username);
+    const passwordHash = hash(password);
     
-    AccountTable.storeAccount({ username, password })
+    AccountTable.storeAccount({ usernameHash, passwordHash })
         .then(() => res.json({ message: 'success!' }))
         .catch(error => next(error));
 });
