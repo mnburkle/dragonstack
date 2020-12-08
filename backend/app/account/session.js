70e64680f5a946c66ca770fafc58a2d24adb5e7f
mnburkle <mnburkle@stanford.edu>
2020-12-07 16:19:35 -0800

    107. session part 2

--- backend/app/account/session.js
+++ backend/app/account/session.js
@@ -1,5 +1,7 @@
 const uuid = require('uuid/v4');
-const hash = require('./helper');
+const { hash } = require('./helper');
+
+const SEPARATOR = '|';
 
 class Session {
     // special constructor executed every time new instance created
@@ -15,14 +17,37 @@ class Session {
         return Session.sessionString({ username, id});
     }
 
+    static parse(sessionString) {
+        const sessionData = sessionString.split(SEPARATOR);
+        return {
+            username: sessionData[0],
+            id: sessionData[1],
+            sessionHash: sessionData[2]
+        };
+    }
+
+    static verify(sessionString) {
+        const { username, id, sessionHash } = Session.parse(sessionString);
+        const accountData = Session.accountData({ username, id });
+        return (sessionHash === hash(accountData));
+    }
+
     static accountData({ username, id }) {
-        return `${username}|${id}`;
+        return `${username}${SEPARATOR}${id}`;
     }
 
     static sessionString({ username, id }) {
         const accountData = Session.accountData({ username, id });
-        return `${accountData}|${hash(accountData)}`;
+        return `${accountData}${SEPARATOR}${hash(accountData)}`;
     }
 }
+
+// // debugging code
+// const foo = new Session({ username: 'foo' });
+// const foostring = foo.toString();
+// console.log("Session.parse(foostring)", Session.parse(foostring));
+// console.log("verify real", Session.verify(foostring));
+// const fakeFooString = `admin_${foostring}`;
+// console.log("verify fake", Session.verify(fakeFooString));
 
 module.exports = Session;
