034c0834faf11be242d6912c4fda6d582261e11a
mnburkle <mnburkle@stanford.edu>
2020-12-16 22:33:55 -0800

    119. account reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,9 +1,12 @@
-import generation from './generation'; // get generationreducer
-import dragon from './dragon'; // get generationreducer
 import { combineReducers } from 'redux'; // key to scalable pattern with redux and reducers
+import account from './account';
+import dragon from './dragon'; // get generationreducer
+import generation from './generation'; // get generationreducer
+
 // take multiple reducers in folder and join them within one overall function
 
 export default combineReducers({
-    generation, 
-    dragon 
+    account, 
+    dragon,
+    generation
 }); // takes in object w every reducer we want to combine. 
