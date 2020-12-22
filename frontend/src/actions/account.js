e794170018776335b6a61ac4fb83a7a060abeb3d
mnburkle <mnburkle@stanford.edu>
2020-12-22 15:29:43 -0800

    120. sign up account action

---
+++ frontend/src/actions/account.js
@@ -0,0 +1,24 @@
+import { ACCOUNT } from './types';
+import { BACKEND } from '../config';
+
+export const signup = ({ username, password }) => dispatch => {
+    dispatch({ type: ACCOUNT.FETCH });
+
+    return fetch(`${BACKEND.ADDRESS}/account/signup`, {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(json => {
+        if (json.type === 'error') {
+            dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message });
+        } else {
+            dispatch({ type: ACCOUNT.FETCH_SUCCESS, ...json }); // mix in everything with spread operator 
+        }
+     })
+    .catch(error => dispatch({ 
+        type: ACCOUNT.FETCH_ERROR, message: error.message 
+    })); // note this is a post req, need extra config for fetch method
+};
