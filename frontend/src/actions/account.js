98c57338f7aa0260ca297a647a2c75f561fc1b9d
mnburkle <mnburkle@stanford.edu>
2020-12-28 15:57:15 -0800

    124. log out action

--- frontend/src/actions/account.js
+++ frontend/src/actions/account.js
@@ -1,6 +1,9 @@
 import { ACCOUNT } from './types';
 import { BACKEND } from '../config';
 
+const fetchFromAccount = ({ endpoint, options, SUCCESS_TYPE}) => {
+}
+
 export const signup = ({ username, password }) => dispatch => {
     dispatch({ type: ACCOUNT.FETCH });
 
@@ -22,3 +25,22 @@ export const signup = ({ username, password }) => dispatch => {
         type: ACCOUNT.FETCH_ERROR, message: error.message 
     })); // note this is a post req, need extra config for fetch method
 };
+
+export const logout = () => dispatch => {
+    dispatch({ type: ACCOUNT.FETCH });
+
+    return fetch(`${BACKEND.ADDRESS}/account/logout`, {
+        credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(json => {
+        if (json.type === 'error') {
+            dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message });
+        } else {
+            dispatch({ type: ACCOUNT.FETCH_LOGOUT_SUCCESS, ...json });
+        }
+    })
+    .catch(error => dispatch({
+        type: ACCOUNT.FETCH_ERROR, message: error.message
+    }));
+}
